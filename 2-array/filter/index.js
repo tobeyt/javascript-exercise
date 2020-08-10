a87bafa111a8efc99d58efa43386b378383e77c0
Tobey <bitsqiu@gmail.com>
2020-08-10 15:20:10 +0800

    [QD]: fixed: 2-array

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,12 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  return collection.filter(cur => cur % 3 === 0);
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  return Array.from(new Set(collection));
+  // return collection.filter((cur, index, arr) => arr.indexOf(cur) === index);
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
--- 2-array/map/index.js
+++ 2-array/map/index.js
@@ -1,3 +1,4 @@
 export default function numberMapToWord(collection) {
   // TODO 3: 在这里写实现代码
+  return collection.map(cur => String.fromCharCode('a'.charCodeAt() + cur - 1));
 }

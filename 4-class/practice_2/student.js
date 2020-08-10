dfce1412d66c5cb10070ef75c58b76dc46e4d985
Tobey <bitsqiu@gmail.com>
2020-08-10 15:53:29 +0800

    [QD] fixed: 4-class

--- 4-class/practice_2/student.js
+++ 4-class/practice_2/student.js
@@ -1,1 +1,12 @@
+import Person from '../practice_1/person';
+
 // TODO 11: 在这里写实现代码
+export default class Student extends Person {
+  constructor(name, age, klass) {
+    super(name, age);
+    this.klass = klass;
+  }
+  introduce() {
+    return `I am a Student. I am at Class ${this.klass}.`;
+  }
+}
--- 4-class/practice_3/worker.js
+++ 4-class/practice_3/worker.js
@@ -1,1 +1,8 @@
+import Person from './person';
+
 // TODO 14: 在这里写实现代码
+export default class Worker extends Person {
+  introduce() {
+    return `I am a Worker. I have a job.`;
+  }
+}
